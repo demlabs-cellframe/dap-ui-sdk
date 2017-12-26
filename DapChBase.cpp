13cdd7c454f067c060041d285c2d65fedc7b515b
Dmitriy Gerasimov <dm@cifercom.com>
2017-12-26 16:00:47 +0700

    [+] LGPLv3 headers

--- DapChBase.cpp
+++ DapChBase.cpp
@@ -9,10 +9,10 @@
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    DAP is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
     along with any DAP based project.  If not, see <http://www.gnu.org/licenses/>.
