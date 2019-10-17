abd55f41c4660d1e7101161616c537b3107d9abc
Dmitriy A. Gerasimov <dmitriy.gerasimov@demlabs.net>
2019-10-17 23:49:28 +0700

    [*] Fixed logger

--- DapLogger.cpp
+++ DapLogger.cpp
@@ -48,8 +48,15 @@ void DapLogger::messageHandler(QtMsgType type,
         fileName = (fileName == Q_NULLPTR ? ctx.file : fileName + 1);
         strcpy(prefixBuffer, fileName);
         sprintf(strrchr(prefixBuffer, '.'), ":%d", ctx.line);
+#ifdef DAP_LOG_MT
         _log_it(prefixBuffer, strlen(prefixBuffer)-1, castQtMsgToDap(type), msg.toLatin1().data());
     } else {
         _log_it("\0",1,  castQtMsgToDap(type), msg.toLatin1().data());
     }
+#else
+        _log_it2(prefixBuffer, castQtMsgToDap(type), msg.toLatin1().data());
+    } else {
+        _log_it2("\0", castQtMsgToDap(type), msg.toLatin1().data());
+    }
+#endif
 }
