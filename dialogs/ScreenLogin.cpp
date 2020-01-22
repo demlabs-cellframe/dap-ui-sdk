e333576197eadeb7ba27d162001b9e3c66e37c8d
alexander.mruchok <alexander.mruchok@demlabs.net>
2020-01-22 19:30:07 +0200

    [*] fixed forms bug

--- dialogs/ScreenLogin.cpp
+++ dialogs/ScreenLogin.cpp
@@ -16,7 +16,11 @@ ScreenLogin::ScreenLogin(QObject * a_parent, QStackedWidget * a_sw)
 {
     
     // Инициализация формы ( в зависимости от размера экрана)
+#ifdef Q_OS_ANDROID
     create<Ui::Login>();
+#else
+    create<Ui::Login, Ui::LoginSmall, Ui::LoginBig>();
+#endif
 /*
     QWidget * cbUpstreamHor=getWidget("cbUpstream",Hor);
     QWidget * cbUpstreamVer=getWidget("cbUpstream",Ver);
