d482500451cbb48c1b05536b3b83b9997fe7dcda
alexander.mruchok <alexander.mruchok@demlabs.net>
2020-01-29 14:14:48 +0200

    [*] Changed rounding

--- auxiliary/UiScaling.cpp
+++ auxiliary/UiScaling.cpp
@@ -8,7 +8,7 @@ int UiScaling::pointsToPixels(float a_points)
 {
     static auto dpi(QGuiApplication::primaryScreen()->physicalDotsPerInch());
     static auto pixelsInPoint(devicePointSize() * dpi);
-    return qRound(a_points * pixelsInPoint);
+    return /*qRound*/(a_points * pixelsInPoint);
 }
 
 QSize UiScaling::pointsToPixels(const QSize &a_pointsSize)
