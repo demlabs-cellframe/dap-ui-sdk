c3be10a42f352d21bf192720073dc4731650ccda
alexandr.mruchok <alexandr.mruchok@demlabs.net>
2020-02-05 23:17:55 +0200

    [*] Changed StartScreen

--- dialogs/StartScreen.cpp
+++ dialogs/StartScreen.cpp
@@ -1,21 +1,42 @@
 #include "StartScreen.h"
 
 #include "StyledDropShadowEffect.h"
+#include "AppController.h"
 
-StartScreen::StartScreen(QObject * a_parent, QStackedWidget * a_sw)
-    : DapUiScreen(a_parent, a_sw)
+//const QString StartScreen::SCREEN_NAME = "StartScreen";
+
+StartScreen::StartScreen(QWidget *a_parent)
+    : Screen(a_parent)
 {
     create<Ui::StartScreen>();
 }
 
-void StartScreen::initUi(QWidget *a_w, DapUiScreen::ScreenRotation a_rotation)
+//void StartScreen::activateScreen()
+//{
+//    AppController::mainWindow()->
+//}
+
+//QString StartScreen::screenName()
+//{
+//    return SCREEN_NAME;
+//}
+
+void StartScreen::initUi(QWidget *a_w, ScreenInfo::Rotation a_rotation)
 {
-    QPushButton *btnSignIn = a_w->findChild<QPushButton*>("btnSignIn");
-    Q_ASSERT(btnSignIn);
+    btnSignIn = a_w->findChild<QPushButton*>("btnSignIn"); Q_ASSERT(btnSignIn);
+    btnSignUp = a_w->findChild<QPushButton*>("btnSignUp"); Q_ASSERT(btnSignUp);
 
     btnSignIn->setGraphicsEffect(new StyledDropShadowEffect(btnSignIn));
 
     connect(btnSignIn, &QPushButton::clicked, [this]{
-        emit this->transitionTo_Login();
+        emit this->transitionTo_SignIn();
     });
+}
+
+void StartScreen::setupStateMachine()
+{
+//    QList<QWidget*> widgets {btnSignIn, btnSignUp};
+//    QStringList widgetNames {"edtEmail", "edtPassword", "btnSignIn", "lblSignUp", "lblForgetPass"}
+//    assignWidgetPropertyForState<QWidget>(AppController::stateMachine()->start, )
+
 }
--- dialogs/StartScreen.h
+++ dialogs/StartScreen.h
@@ -2,11 +2,11 @@
 #define STARTSCREEN_H
 
 #include "DapUiScreen.h"
+#include "Screen.h"
 
 #include "ui_StartScreen.h"
 
-
-class StartScreen : public DapUiScreen
+class StartScreen : public Screen
 {
     Q_OBJECT
 
@@ -14,17 +14,27 @@ public:
     /// Overloaded constructor.
     /// @param a_parent Parent.
     /// @param a_sw Application window stack.
-    StartScreen(QObject * a_parent, QStackedWidget * a_sw);
+    StartScreen(QWidget * a_parent);
+
+//    virtual void activateScreen() override;
+
+//    virtual QString screenName() override;
+//    static const QString SCREEN_NAME;
 
 protected:
     /// Form initialization.
     /// @param a_w Window GUI widget.
     /// @param a_rotation Device display orientation.
-    virtual void initUi(QWidget * a_w, ScreenRotation a_rotation) override;
+    virtual void initUi(QWidget * a_w, ScreenInfo::Rotation a_rotation) override;
+
+    QPushButton *btnSignIn;
+    QPushButton *btnSignUp;
 
 signals:
-    void transitionTo_Login();
+    void transitionTo_SignIn();
 
+private:
+    void setupStateMachine();
 };
 
 #endif // STARTSCREEN_H
