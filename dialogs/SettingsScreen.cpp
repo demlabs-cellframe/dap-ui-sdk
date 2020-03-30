bb03b9287b440914e21893481356c77967fa00cc
konstantin.kukharenko <konstantin.kukharenko@demlabs.net>
2020-03-30 20:03:00 +0300

    [*]Edit screen

--- dialogs/SettingsScreen.cpp
+++ dialogs/SettingsScreen.cpp
@@ -10,30 +10,35 @@ SettingsScreen::SettingsScreen(QWidget *a_parent)
 
 void SettingsScreen::initVariantUi(QWidget *a_widget)
 {
-    CustomComboBox          *cbbLanguage    = a_widget->findChild<CustomComboBox*>(CBB_LANGUAGE);          Q_ASSERT(cbbLanguage);
+
 
 #ifdef Q_OS_ANDROID
-
+    CustomComboBox          *cbbLanguage    = a_widget->findChild<CustomComboBox*>(CBB_LANGUAGE);           Q_ASSERT(cbbLanguage);
     CustomPlacementButton   *btnLogOut      = a_widget->findChild<CustomPlacementButton*>(BTN_LOG_OUT);     Q_ASSERT(btnLogOut);
-    CustomPlacementButton   *btnRenew       = a_widget->findChild<CustomPlacementButton*>(BTN_RENEW);      Q_ASSERT(btnRenew);
+    CustomPlacementButton   *btnRenew       = a_widget->findChild<CustomPlacementButton*>(BTN_RENEW);       Q_ASSERT(btnRenew);
 
     //btnRenew->spacerActivate(ButtonSpaicer::Left);
     //btnLogOut->spacerActivate(ButtonSpaicer::Left);
 
     cbbLanguage->addItem("Language");
 #else
-    CustomComboBox *cbbBalance      = a_widget->findChild<CustomComboBox*>(CBB_BALABCE);       Q_ASSERT(cbbBalance);
-    CustomComboBox *cbbBandwidth    = a_widget->findChild<CustomComboBox*>(CBB_BANDWIDTH);     Q_ASSERT(cbbBandwidth);
-    CustomComboBox *cbbEncryption   = a_widget->findChild<CustomComboBox*>(CBB_ENCRYPTION);    Q_ASSERT(cbbEncryption);
-    CustomComboBox *cbbUptime       = a_widget->findChild<CustomComboBox*>(CBB_UPTIME);        Q_ASSERT(cbbUptime);
+    CustomButtonComboBox *cbbBalance      = a_widget->findChild<CustomButtonComboBox*>(CBB_BALABCE);       Q_ASSERT(cbbBalance);
+    CustomButtonComboBox *cbbBandwidth    = a_widget->findChild<CustomButtonComboBox*>(CBB_BANDWIDTH);     Q_ASSERT(cbbBandwidth);
+    CustomButtonComboBox *cbbEncryption   = a_widget->findChild<CustomButtonComboBox*>(CBB_ENCRYPTION);    Q_ASSERT(cbbEncryption);
+    CustomButtonComboBox *cbbUptime       = a_widget->findChild<CustomButtonComboBox*>(CBB_UPTIME);        Q_ASSERT(cbbUptime);
+    CustomButtonComboBox *cbbLanguage    = a_widget->findChild<CustomButtonComboBox*>(CBB_LANGUAGE);       Q_ASSERT(cbbLanguage);
 
+    cbbBandwidth->setButtonControll(new CustomPlacementButton(ImagePos::Right));
+    cbbUptime->setButtonControll(new CustomPlacementButton(ImagePos::Right));
+    cbbEncryption->setButtonControll(new CustomPlacementButton(ImagePos::Right));
+    cbbBalance->setButtonControll(new CustomPlacementButton(ImagePos::Right));
+    cbbLanguage->setButtonControll(new CustomPlacementButton(ImagePos::Right));
 
-
-    cbbBandwidth->addItem("Bandwidth");
-    cbbUptime->addItem("Uptime");
-    cbbEncryption->addItem("Encryption");
-    cbbBalance->addItem("Balance");
-    cbbLanguage->addItem("Language");
+    cbbBandwidth->setCaption("Bandwidth");
+    cbbUptime->setCaption("Uptime");
+    cbbEncryption->setCaption("Encryption");
+    cbbBalance->setCaption("Balance");
+    cbbLanguage->setCaption("Language");
 #endif
 }
 QString SettingsScreen::screenName()
