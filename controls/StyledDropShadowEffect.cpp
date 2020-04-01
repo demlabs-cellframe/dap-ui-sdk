9f1d355b7026887c58e3bae75515678c3866648e
konstantin.kukharenko <konstantin.kukharenko@demlabs.net>
2020-04-01 20:52:31 +0300

    [*]Edit hover state

--- controls/StyledDropShadowEffect.cpp
+++ controls/StyledDropShadowEffect.cpp
@@ -6,17 +6,29 @@
 StyledDropShadowEffect::StyledDropShadowEffect(QObject *a_parent /*= Q_NULLPTR*/)
     :QGraphicsDropShadowEffect(a_parent)
 {
+    defaultShadow = nullptr;
+    hoverShadow = nullptr;
+
     updateStyleProperties();
     a_parent->installEventFilter(this);
 
+    connectToParentEvents();
+
+}
+
+///@details Connects connections
+void StyledDropShadowEffect::connectToParentEvents()
+{
     connect(this,&StyledDropShadowEffect::mouseEnter,[=]{
-       qDebug()<<"mouse Enter";
+        this->updateStyle(StyleShedow::HOVER_SHADOW);
     });
+
     connect(this,&StyledDropShadowEffect::mouseLaeve,[=]{
-       qDebug()<<"mouse Buy Buy";
+        this->updateStyle(StyleShedow::DEFAULT_SHADOW);
     });
-    connect(a_parent,&QObject::objectNameChanged,[=]{
-        qDebug()<<"new Object name";
+
+    connect(this->parent(),&QObject::objectNameChanged,[=]{
+        this->updateStyleProperties();
     });
 }
 
@@ -26,12 +38,21 @@ void StyledDropShadowEffect::updateStyleProperties()
     StyleSheatSearchPar searchPar;
     searchPar.widgetName = "#" + parent()->objectName();
     searchPar.subcontrol = "shadow";
-    setDataShadowProperties(AppStyleSheetHandler::getWidgetStyleSheet(searchPar),defaultShadow);
-
+    if(AppStyleSheetHandler::getWidgetStyleSheet(searchPar) !="")
+    {
+        if(!defaultShadow)
+            defaultShadow = new ShadowProperties();
+        setDataShadowProperties(AppStyleSheetHandler::getWidgetStyleSheet(searchPar),defaultShadow);
+    }
     updateStyle(DEFAULT_SHADOW);
 
     searchPar.pseudoClass = "hover";
-    setDataShadowProperties(AppStyleSheetHandler::getWidgetStyleSheet(searchPar),hoverShadow);
+    if(AppStyleSheetHandler::getWidgetStyleSheet(searchPar) !="")
+    {
+        if(!hoverShadow)
+            hoverShadow = new ShadowProperties();
+        setDataShadowProperties(AppStyleSheetHandler::getWidgetStyleSheet(searchPar),hoverShadow);
+    }
 }
 
 ///@details Setting and setting the default shadow
@@ -39,8 +60,14 @@ void StyledDropShadowEffect::updateStyle(StyleShedow a_style)
 {
     switch (a_style)
     {
-    case DEFAULT_SHADOW:setShadowProperties(defaultShadow);break;
-    case HOVER_SHADOW:setShadowProperties(hoverShadow);break;
+    case DEFAULT_SHADOW:
+        if(defaultShadow)
+            setShadowProperties(defaultShadow);
+                break;
+    case HOVER_SHADOW:
+        if(hoverShadow)
+            setShadowProperties(hoverShadow);
+                break;
     default: break;
     }
 
@@ -51,27 +78,27 @@ void StyledDropShadowEffect::updateStyle(StyleShedow a_style)
 ///@details Saving data to a shadow structure
 /// @param  a_property String with settings from css.
 /// @param data Data structure with shadow settings.
-void StyledDropShadowEffect::setDataShadowProperties(const QString &stylesheet, ShadowProperties &data)
+void StyledDropShadowEffect::setDataShadowProperties(const QString &stylesheet, ShadowProperties* data)
 {
     //Color:
     QString colorStr = AppStyleSheetHandler::getValueFromStylesheet(stylesheet, "color");
     if (colorStr.isEmpty())
-        data.color = QColor(0,0,0,0);
+        data->color = QColor(0,0,0,0);
     else
-        data.color = Utils::toColor(colorStr);
-    data.blur = Utils::toIntValue(AppStyleSheetHandler::getValueFromStylesheet(stylesheet, "blur"));
+        data->color = Utils::toColor(colorStr);
+    data->blur = Utils::toIntValue(AppStyleSheetHandler::getValueFromStylesheet(stylesheet, "blur"));
     //Offset:
-    data.x = Utils::toIntValue(AppStyleSheetHandler::getValueFromStylesheet(stylesheet, "x"));
-    data.y = Utils::toIntValue(AppStyleSheetHandler::getValueFromStylesheet(stylesheet, "y"));
+    data->x = Utils::toIntValue(AppStyleSheetHandler::getValueFromStylesheet(stylesheet, "x"));
+    data->y = Utils::toIntValue(AppStyleSheetHandler::getValueFromStylesheet(stylesheet, "y"));
 }
 
 ///@details Set shadow options
 /// @param data Data structure with shadow settings.
-void StyledDropShadowEffect::setShadowProperties(ShadowProperties &data)
+void StyledDropShadowEffect::setShadowProperties(ShadowProperties *data)
 {
-    this->setColor(data.color);
-    this->setBlurRadius(data.blur);
-    this->setOffset(data.x, data.y);
+    this->setColor(data->color);
+    this->setBlurRadius(data->blur);
+    this->setOffset(data->x, data->y);
 }
 
 bool StyledDropShadowEffect::eventFilter(QObject *watched, QEvent *event)
--- controls/StyledDropShadowEffect.h
+++ controls/StyledDropShadowEffect.h
@@ -33,13 +33,15 @@ class StyledDropShadowEffect : public QGraphicsDropShadowEffect
 {
     Q_OBJECT
 public:
-    StyledDropShadowEffect(QObject *a_parent = Q_NULLPTR);
+    StyledDropShadowEffect(QObject *a_parent);
     ///@details Setting the shadow
     void updateStyle(StyleShedow a_style);
     ///@details Collecting data from css
     void updateStyleProperties();
 protected:
     bool eventFilter(QObject *watched, QEvent *event);
+    ///@details Connects connections
+    void connectToParentEvents();
 signals:
     void mouseEnter();
     void mouseLaeve();
@@ -47,14 +49,14 @@ private:
     ///@details Saving data to a shadow structure
     /// @param  a_property String with settings from css.
     /// @param data Data structure with shadow settings.
-    void setDataShadowProperties(const QString &a_property, ShadowProperties &data);
+    void setDataShadowProperties(const QString &a_property, ShadowProperties *data);
     ///@details Set shadow options
     /// @param data Data structure with shadow settings.
-    void setShadowProperties(ShadowProperties &data);
+    void setShadowProperties(ShadowProperties *data);
     ///@details Default Shadow Data
-    ShadowProperties defaultShadow;
+    ShadowProperties *defaultShadow;
     ///@details Hover Shadow Data
-    ShadowProperties hoverShadow;
+    ShadowProperties *hoverShadow;
 };
 
 #endif // STYLEDDROPSHADOWEFFECT_H
