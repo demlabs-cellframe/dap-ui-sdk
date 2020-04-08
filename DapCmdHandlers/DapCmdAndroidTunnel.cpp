e6075f3b7da1428233fb7c6042178f80243b71ad
alexandr.mruchok <alexandrmruchok@demlabs.net>
2020-04-08 23:10:49 +0300

    [+] Added log Info

--- DapCmdHandlers/DapCmdAndroidTunnel.cpp
+++ DapCmdHandlers/DapCmdAndroidTunnel.cpp
@@ -19,6 +19,7 @@ void DapCmdAndroidTunnel::handle(const QJsonObject *params)
 
 void DapCmdAndroidTunnel::openTunnel(const QString &address, const QString &gateway, int socket)
 {
+    qDebug() << QString("openTunnel(%1,%2,%3)").arg(address).arg(gateway).arg(socket);
     QJsonObject tunnelData;
     tunnelData["address"] = address;
     tunnelData["gateway"] = gateway;
