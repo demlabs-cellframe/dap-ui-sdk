c0c0f984a0b7f7b3e489bd0a5d5a6ccd08fb40e5
konstantin.kukharenko <konstantin.kukharenko@demlabs.net>
2020-04-15 16:05:54 +0300

    [*]Reload effect

--- controls/ScreenOverlaying.cpp
+++ controls/ScreenOverlaying.cpp
@@ -1,14 +1,54 @@
 #include "ScreenOverlaying.h"
+#include <QDebug>
 
-ScreenOverlaying::ScreenOverlaying(QWidget *parent):QWidget(parent) {
+ScreenOverlaying::ScreenOverlaying(QWidget *a_parent)
+    :QWidget(a_parent),
+    blurRadius(0),
+    opacity(0)
+{
+
 }
 
-void ScreenOverlaying::mousePressEvent(QMouseEvent *event){
-    Q_UNUSED(event);
-    emit mousePressed();
+ScreenOverlaying::ScreenOverlaying(QWidget *blure_parent, QWidget *opacity_parent)
+    :QWidget(opacity_parent),
+    blurRadius(0),
+    opacity(0)
+{
+    setObjectName("ScreenBlureOpocityEffect");
+    m_blurParent = blure_parent;
+    m_opacityParent = opacity_parent;
+
+    this->setVisible(false);
 }
 
-void ScreenOverlaying::paintEvent(QPaintEvent *event){
-    QPainter painter(this);
-    painter.fillRect(event->rect(), palette().color(QPalette::Window));
+void ScreenOverlaying::showEvent(QShowEvent *event)
+{
+    QGraphicsBlurEffect *blurEffect = new QGraphicsBlurEffect(m_blurParent);
+    blurEffect->setBlurRadius(blurRadius);
+    m_blurParent->setGraphicsEffect(blurEffect);
+
+    setGeometry(0,0,m_opacityParent->width(),m_opacityParent->height());
+    QGraphicsOpacityEffect *opacityEffect = new QGraphicsOpacityEffect(this);
+    opacityEffect->setOpacity(opacity);
+
+    setGraphicsEffect(opacityEffect);
+
+
+    QWidget::showEvent(event);
+}
+
+void ScreenOverlaying::hideEvent(QHideEvent *event)
+{
+    delete m_blurParent->graphicsEffect();
+    delete graphicsEffect();
+    QWidget::hideEvent(event);
+}
+void ScreenOverlaying::setBlurRadius(int &a_blurRadius)
+{
+    blurRadius = a_blurRadius;
+}
+
+void ScreenOverlaying::setOpacity(qreal &a_opacity)
+{
+    opacity = a_opacity;
 }
--- controls/ScreenOverlaying.h
+++ controls/ScreenOverlaying.h
@@ -2,24 +2,49 @@
 #define SCREENOVERLAYING_H
 
 #include <QWidget>
-#include <QPainter>
-#include <QPaintEvent>
+#include <QGraphicsOpacityEffect>
+#include <QGraphicsBlurEffect>
 
-//claass wich fill screen under menu
+/* css style
+ #ScreenBlureOpocityEffect
+{
+    background-color: color
+    qproperty-blureRadius: integer property
+    qproperty-opocity: double property 0...1
+}
+
+ */
 class ScreenOverlaying: public QWidget
 {
     Q_OBJECT
+    Q_PROPERTY(int blurRadius WRITE setBlurRadius DESIGNABLE true)
+    Q_PROPERTY(qreal opacity WRITE setOpacity DESIGNABLE true)
 public:
-    ScreenOverlaying(QWidget *parent = nullptr);
+    ScreenOverlaying(QWidget *a_parent);
+
+    /// Constructor with two widgets specified. 1st widget for creating blurring.
+    ///  The second widget must be the parent of the first and the parent
+    /// of this class to create a transparent background.
+    /// @param blure_parent.
+    /// @param opocity_parent
+    ScreenOverlaying(QWidget *blur_parent, QWidget *opacity_parent);
 
 protected:
+    void showEvent(QShowEvent *event) override;
+    void hideEvent(QHideEvent *event) override;
+    /// Setting a parameter for the BlurEffect.
+    /// @param a_blureRadius.
+    void setBlurRadius(int &a_blurRadius);
+    /// Setting a parameter for the OpacityEffect.
+    /// @param a_opocity.
+    void setOpacity(qreal &a_opacity);
 
-    virtual void mousePressEvent(QMouseEvent *event);
-    virtual void paintEvent(QPaintEvent *event);
+private:
 
-signals:
-    void mousePressed();
+   QWidget* m_blurParent;
+   QWidget* m_opacityParent;
+
+   int blurRadius;
+   qreal opacity;
 };
-
-
 #endif // SCREENOVERLAYING_H
