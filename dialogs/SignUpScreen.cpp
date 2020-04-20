5459b465c08e6a3045dad4954fc30e78f9b1cc64
Alexandr Mruchok <alexandrmruchok@gmail.com>
2020-04-20 17:13:49 +0000

    [*] Change 'chbInfo' to 'chbAgree', 'transfornTo_SignIn()' to 'goToSignIn()', change controls states and styles

--- dialogs/SignUpScreen.cpp
+++ dialogs/SignUpScreen.cpp
@@ -10,11 +10,42 @@ SignUpScreen::SignUpScreen(QWidget *a_parent)
     create<Ui::SignUpScreen>();
 
     initScreen(this);
+
+    connect(this, SIGNAL(emailEdited        (const QString &)), SLOT(setEmail       (const QString &)));
+    connect(this, SIGNAL(passwordEdited     (const QString &)), SLOT(setPassword    (const QString &)));
+    connect(this, SIGNAL(repeatPasswordEdited(const QString &)), SLOT(setRptPassword(const QString &)));
+    connect(this, SIGNAL(agreeChanged       (const bool &)),    SLOT(setAgree       (const bool &))    );
+
+
 }
 
 QString SignUpScreen::screenName()
 {
     return SignUpScreen::SCREEN_NAME;
+}
+
+void SignUpScreen::setEmail(const QString &a_email)
+{
+    setChildProperties(EDT_EMAIL, "text", a_email);
+    m_email = a_email;
+}
+
+void SignUpScreen::setPassword(const QString &a_password)
+{
+    setChildProperties(EDT_PASSWORD, "text", a_password);
+    m_password = a_password;
+}
+
+void SignUpScreen::setRptPassword(const QString &a_rpt_password)
+{
+    setChildProperties(EDT_RPT_PASSWORD, "text", a_rpt_password);
+    m_repeat_password = a_rpt_password;
+}
+
+void SignUpScreen::setAgree(const bool &a_agree)
+{
+    setChildProperties(CHB_AGREE, "bool", a_agree);
+    m_agree = a_agree;
 }
 
 void SignUpScreen::initVariantUi(QWidget *a_widget)
@@ -27,7 +58,7 @@ void SignUpScreen::initVariantUi(QWidget *a_widget)
     CustomLineEdit          *edtEmail               = a_widget->findChild<CustomLineEdit*>(EDT_EMAIL);                  Q_ASSERT(edtEmail);
     PasswordLineEdit        *edtPassword            = a_widget->findChild<PasswordLineEdit*>(EDT_PASSWORD);             Q_ASSERT(edtPassword);
     PasswordLineEdit        *edtRptPassword         = a_widget->findChild<PasswordLineEdit*>(EDT_RPT_PASSWORD);         Q_ASSERT(edtRptPassword);
-    QCheckBox               *chbInfo                = a_widget->findChild<QCheckBox*>(CHB_INFO);                        Q_ASSERT(chbInfo);
+    QCheckBox               *chbAgree                = a_widget->findChild<QCheckBox*>(CHB_AGREE);                      Q_ASSERT(chbAgree);
     QLabel                  *lblInfo                = a_widget->findChild<QLabel*>(LBL_INFO);                           Q_ASSERT(lblInfo);
 
     QWidget                 *wgtInfo                = a_widget->findChild<QWidget*>(WGT_INFO);                          Q_ASSERT(wgtInfo);
@@ -79,25 +110,124 @@ void SignUpScreen::initVariantUi(QWidget *a_widget)
     edtPassword->setEchoMode(QLineEdit::Password);
     edtRptPassword->setPlaceholderText("Repeat password");
     edtRptPassword->setEchoMode(QLineEdit::Password);
+    //chbAgree->setChecked(false);
 
-
-    connect(btnSignUp,&QPushButton::clicked,[=]{
-
+    connect(this, &SignUpScreen::wrongEmail, [=](){
         lblEmailError->setVisible(true);
-        lblPasswordError->setVisible(true);
-        lblRptPasswordError->setVisible(true);
-
-        Utils::setPropertyAndUpdateStyle(edtEmail,Properties::STATE,Properties::ERROR);
-        Utils::setPropertyAndUpdateStyle(wgtEmailBottom,Properties::STATE,Properties::ERROR);
-
-
-        Utils::setPropertyAndUpdateStyle(edtPassword,Properties::STATE,Properties::ERROR);
-        Utils::setPropertyAndUpdateStyle(wgtFirstPasswordBottom,Properties::STATE,Properties::ERROR);
-
-        Utils::setPropertyAndUpdateStyle(edtRptPassword,Properties::STATE,Properties::ERROR);
-        Utils::setPropertyAndUpdateStyle(wgtSecondPasswordBottom,Properties::STATE,Properties::ERROR);
+        Utils::setPropertyAndUpdateStyle(edtEmail, Properties::ERROR, true);
+        Utils::setPropertyAndUpdateStyle(wgtEmailBottom, Properties::ERROR, true);
     });
+    connect(this, &SignUpScreen::wrongPassword, [=](){
+        lblPasswordError->setVisible(true);
+        Utils::setPropertyAndUpdateStyle(edtPassword, Properties::ERROR, true);
+        Utils::setPropertyAndUpdateStyle(wgtFirstPasswordBottom, Properties::ERROR, true);
+    });
+    connect(this, &SignUpScreen::wrongRepeatPassword, [=](){
+        lblRptPasswordError->setVisible(true);
+        Utils::setPropertyAndUpdateStyle(edtRptPassword, Properties::ERROR, true);
+        Utils::setPropertyAndUpdateStyle(wgtSecondPasswordBottom, Properties::ERROR, true);
+    });
+
+    connect(this, &SignUpScreen::agreeChanged, [=](){
+        Utils::setPropertyAndUpdateStyle(chbAgree, Properties::WRONG, false);
+    });
+    connect(this, &SignUpScreen::notAgree, [=](){
+        Utils::setPropertyAndUpdateStyle(chbAgree, Properties::WRONG, true);
+    });
+
+    connect(edtEmail, &CustomLineEdit::textChanged, [=](){
+        lblEmailError->setVisible(false);
+        Utils::setPropertyAndUpdateStyle(edtEmail, Properties::ERROR, false);
+        Utils::setPropertyAndUpdateStyle(wgtEmailBottom, Properties::ERROR, false);
+    });
+    connect(edtPassword, &CustomLineEdit::textChanged, [=](){
+        lblPasswordError->setVisible(false);
+        Utils::setPropertyAndUpdateStyle(edtPassword, Properties::ERROR, false);
+        Utils::setPropertyAndUpdateStyle(wgtFirstPasswordBottom, Properties::ERROR, false);
+        lblRptPasswordError->setVisible(false);
+        Utils::setPropertyAndUpdateStyle(edtRptPassword, Properties::ERROR, false);
+        Utils::setPropertyAndUpdateStyle(wgtSecondPasswordBottom, Properties::ERROR, false);
+    });
+    connect(edtRptPassword, &CustomLineEdit::textChanged, [=](){
+        lblPasswordError->setVisible(false);
+        Utils::setPropertyAndUpdateStyle(edtPassword, Properties::ERROR, false);
+        Utils::setPropertyAndUpdateStyle(wgtFirstPasswordBottom, Properties::ERROR, false);
+        lblRptPasswordError->setVisible(false);
+        Utils::setPropertyAndUpdateStyle(edtRptPassword, Properties::ERROR, false);
+        Utils::setPropertyAndUpdateStyle(wgtSecondPasswordBottom, Properties::ERROR, false);
+    });
+
 
 #endif
 
+    connect(lblSignIn,          SIGNAL(clicked()),              this, SIGNAL(goToSignIn())         );
+    connect(lblTerms,           SIGNAL(clicked()),              this, SIGNAL(transitionToTerms())           );
+    connect(lblPrivacyPolisy,   SIGNAL(clicked()),              this, SIGNAL(transitionToPrivacyPolicy())   );
+
+    connect(edtEmail,           SIGNAL(textEdited(QString)),    this, SIGNAL(emailEdited(QString))          );
+    connect(edtPassword,        SIGNAL(textEdited(QString)),    this, SIGNAL(passwordEdited(QString))       );
+    connect(edtRptPassword,     SIGNAL(textEdited(QString)),    this, SIGNAL(repeatPasswordEdited(QString)) );
+    connect(chbAgree,           SIGNAL(clicked(bool)),          this, SIGNAL(agreeChanged(bool))            );
+
+    connect(btnSignUp,          SIGNAL(clicked(bool)),          this, SLOT(checkFieldsAndSignUp())          );
+
+
+
+}
+
+bool SignUpScreen::checkEmail()
+{
+    if (m_email.isEmpty() || m_email.contains(' ') || !m_email.contains('@'))
+    {
+        emit this->wrongEmail();
+        return false;
+    }
+    else
+        return true;
+}
+
+bool SignUpScreen::checkPassword()
+{
+    if (m_password.isEmpty() || m_password.contains(' '))
+    {
+        emit this->wrongPassword();
+        return false;
+    }
+    else
+        return true;
+}
+
+bool SignUpScreen::checkRepeatPassword()
+{
+    if (m_repeat_password != m_password || m_repeat_password.isEmpty())
+    {
+        emit this->wrongRepeatPassword();
+        return false;
+    }
+    else
+        return true;
+}
+
+bool SignUpScreen::checkAgree()
+{
+    if (!m_agree)
+    {
+        emit this->notAgree();
+        return false;
+    }
+    else
+        return true;
+}
+
+void SignUpScreen::checkFieldsAndSignUp()
+{
+    qDebug() << "checkFieldsAndSignUp";
+
+    bool emailIsValid    = checkEmail();
+    bool passwordIsValid = checkPassword();
+    bool repeatPasswordIsValid = checkRepeatPassword();
+    bool agreeIsValid = checkAgree();
+
+    if (emailIsValid && passwordIsValid && repeatPasswordIsValid && agreeIsValid)
+        emit this->signUpRequest();
 }
--- dialogs/SignUpScreen.h
+++ dialogs/SignUpScreen.h
@@ -1,6 +1,10 @@
 #ifndef SIGNUPSCREEN_H
 #define SIGNUPSCREEN_H
 
+#include <QLineEdit>
+#include <QLabel>
+#include <QStateMachine>
+
 #include <QCheckBox>
 #include "AdaptiveScreen.h"
 #include "StyledDropShadowEffect.h"
@@ -22,6 +26,16 @@ public:
     static const QString SCREEN_NAME;
     virtual QString screenName() override;
 
+
+public slots:
+
+    void setEmail   (const QString &a_email   );
+    void setPassword(const QString &a_password);
+    void setRptPassword(const QString &a_rpt_password);
+    void setAgree(const bool &a_agree);
+
+    void checkFieldsAndSignUp();
+
 protected:
 
     /// Form initialization.
@@ -30,10 +44,35 @@ protected:
     virtual void initVariantUi(QWidget *a_widget) override;
 
 signals:
-    void transitionTo_SignIn();
+
+    void goToSignIn();
+    void transitionToTerms();
+    void transitionToPrivacyPolicy();
+
+    void wrongEmail();
+    void wrongPassword();
+    void wrongRepeatPassword();
+    void notAgree();
+
+    void emailEdited   (const QString &email   );
+    void passwordEdited(const QString &password);
+    void repeatPasswordEdited(const QString &repeatPassword);
+    void agreeChanged(const bool &agree);
+
+    void signUpRequest();
 
 
 private:
+
+    bool checkEmail();
+    bool checkPassword();
+    bool checkRepeatPassword();
+    bool checkAgree();
+
+    QString m_email;
+    QString m_password;
+    QString m_repeat_password;
+    bool    m_agree;
 
     const QString BTN_SIGN_UP                   = "btnSignUp";
     const QString LBL_TERMS                     = "lblTerms";
@@ -42,7 +81,7 @@ private:
     const QString EDT_EMAIL                     = "edtEmail";
     const QString EDT_PASSWORD                  = "edtPassword";
     const QString EDT_RPT_PASSWORD              = "edtRptPassword";
-    const QString CHB_INFO                      = "chbInfo";
+    const QString CHB_AGREE                     = "chbAgree";
     const QString LBL_INFO                      = "lblInfo";
     const QString LBL_RPT_PASSWORD_ERROR        = "lblRptPasswordError";
     const QString LBL_PASSWORD_ERROR            = "lblPasswordError";
