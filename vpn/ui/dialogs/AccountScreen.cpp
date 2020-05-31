b6fe4ba5baaaec0e4ecce9721d4f3968bc64ec54
konstantin.kukharenko <konstantin.kukharenko@demlabs.net>
2020-05-31 20:43:38 +0300

    [*]Account screen

--- vpn/ui/dialogs/AccountScreen.cpp
+++ vpn/ui/dialogs/AccountScreen.cpp
@@ -26,30 +26,3 @@ void AccountScreen::initVariantUi(QWidget *a_widget)
     Q_UNUSED(a_widget)
 
 }
-//AccountScreen::AccountScreen(QObject * a_parent, QStackedWidget * a_sw)
-//    : DapUiScreen(a_parent, a_sw)
-//{
-//    create<Ui::AccountScreen>();
-
-//    QLabel *lblSwitchLeftText = a_sw->findChild<QLabel*>("lblSwitchLeftText");
-//    QLabel *lblSwitchRightText = a_sw->findChild<QLabel*>("lblSwitchRightText");
-//    QPushButton *btnSwitch = a_sw->findChild<QPushButton*>("btnSwitchButton");
-//    QPushButton *btnChangePlan  = a_sw->findChild<QPushButton*>("btnChangePlan");
-
-//    Q_ASSERT(lblSwitchLeftText);
-//    Q_ASSERT(lblSwitchRightText);
-//    Q_ASSERT(btnSwitch);
-//    Q_ASSERT(btnChangePlan);
-
-//    btnChangePlan->setGraphicsEffect(new StyledDropShadowEffect(btnChangePlan));
-
-//    ///The default Radiobutton value for checking the operation
-//    btnSwitch->setChecked(false);
-//    lblSwitchLeftText->setEnabled(false);
-
-//    connect(btnSwitch,&QPushButton::clicked,[=]{
-//        lblSwitchLeftText->setEnabled(btnSwitch->isChecked());
-//        lblSwitchRightText->setEnabled(!btnSwitch->isChecked());
-//    });
-//}
-
