5b2fefa41f05353ea80d441ca428a9e93526facf
Postnickov <postnickov.lyoha@yandex.ru>
2020-09-15 15:04:55 +0300

    [*]NewsFeedWidget use QPropertyAnimation

--- ui/controls/NewsFeedWidget.cpp
+++ ui/controls/NewsFeedWidget.cpp
@@ -2,10 +2,11 @@
 
 NewsFeedWidget::NewsFeedWidget(QString a_text, QString a_url, int a_speed, QWidget *parent)
         :QLabel(parent),
-        m_parent(parent)
+          m_url(a_url)
 {
     m_layout = new QHBoxLayout(this);
-    m_lblText = new RunLineLabel(a_text, a_url,this);
+    m_lblText = new ClickableRunningLabel(a_text, this);
+    m_lblText->setObjectName("lblTextNews");
     setSpeed(a_speed);
 
     m_lblClose = new ClickableLabel(this);
@@ -18,6 +19,12 @@ NewsFeedWidget::NewsFeedWidget(QString a_text, QString a_url, int a_speed, QWidg
     {
         this->close();
     });
+
+    connect(m_lblText,&ClickableLabel::clicked,[this]()
+    {
+        if (!m_url.isEmpty()) QDesktopServices::openUrl(QUrl(m_url));
+    });
+
     m_layout->setContentsMargins(0,0,0,0);
     m_layout->setSpacing(0);
     setLayout(m_layout);
@@ -25,7 +32,7 @@ NewsFeedWidget::NewsFeedWidget(QString a_text, QString a_url, int a_speed, QWidg
 
 void NewsFeedWidget::updateGeometry()
 {
-    this->setGeometry(0,m_parent->height()-this->size().rheight(),this->width(),0);
+    this->setGeometry(0,qobject_cast<QWidget*>(this->parent())->height() - this->size().rheight(),this->width(),0);
     if (!m_spacerFirst && !m_spacerSecond)
     {
         m_spacerFirst = new QSpacerItem((this->width() - (m_lblText->width() + m_lblClose->width()))/2,0);
@@ -35,14 +42,9 @@ void NewsFeedWidget::updateGeometry()
     }
 }
 
-void NewsFeedWidget::setUrl(const QString &a_url)
-{
-    m_lblText->setUrl(a_url);
-}
-
 void NewsFeedWidget::setText(const QString &a_text)
 {
-    m_lblText->RunLineLabel::setText(a_text);
+    m_lblText->ClickableRunningLabel::setText(a_text);
 }
 
 void NewsFeedWidget::setSpeed(const int a_speed)
@@ -52,115 +54,11 @@ void NewsFeedWidget::setSpeed(const int a_speed)
 
 QString NewsFeedWidget::text() const
 {
-    return m_lblText->RunLineLabel::text();
+    return m_lblText->ClickableRunningLabel::text();
 }
 
 void NewsFeedWidget::resizeEvent(QResizeEvent *ev)
 {
     updateGeometry();
     QLabel::resizeEvent(ev);
-}
-
-
-//*****************RunLineLabel*****************
-RunLineLabel::RunLineLabel(QString a_text, QString a_url, QWidget* a_parent)
-    :ClickableLabel(a_parent),
-      m_text(a_text),
-      m_url(a_url),
-      m_parent(a_parent)
-{
-    QLabel::setText(a_text);
-    setObjectName("lblTextNews");
-    m_timer = new QTimer(this);
-    m_timer->setInterval(30);
-    connect(m_timer, SIGNAL(timeout()), this, SLOT(timeoutAnimation()));
-
-    connect(this, &RunLineLabel::startAnimation, [this]()
-    {
-        if(!m_layout)
-        {
-            m_layout = new QHBoxLayout(this);
-
-            m_lblFirst = new QLabel(this->text(),this);
-            m_lblSecond = new QLabel(this->text(),this);
-
-            m_layout->addWidget(m_lblFirst);
-            m_layout->addWidget(m_lblSecond);
-            m_layout->setContentsMargins(0,0,0,0);
-            m_layout->setSpacing(0);
-            this->setLayout(m_layout);
-
-            QLabel::setText("");
-        }
-        if (!m_timer->isActive()) m_timer->start();
-    });
-    connect(this, &RunLineLabel::stopAnimation, [this]()
-    {
-        if (m_timer->isActive()) m_timer->stop();
-        if (m_layout)
-        {
-            this->QLabel::setText(m_lblFirst->text());
-            delete m_lblSecond;
-            delete m_lblFirst;
-            delete m_layout;
-        }
-    });
-
-    connect(this,&ClickableLabel::clicked,[this]()
-    {
-        if (!m_url.isEmpty()) QDesktopServices::openUrl(QUrl(m_url));
-    });
-    setAlignment(Qt::AlignHCenter);
-}
-
-void RunLineLabel::setText(const QString &a_text)
-{
-    m_text = a_text;
-    this->QLabel::setText(a_text);
-    verifyWidth();
-}
-
-void RunLineLabel::setSpeed(const int a_speed)
-{
-    if (a_speed <= 0) m_speed = 2;
-    else if (a_speed) m_speed = a_speed;
-}
-
-void RunLineLabel::verifyWidth()
-{
-    QFontMetrics fm(this->font());
-    m_widthText = fm.width(m_text);
-    if (m_widthText > this->width()) emit startAnimation();
-    else emit stopAnimation();
-}
-
-void RunLineLabel::showEvent(QShowEvent *e)
-{
-    verifyWidth();
-    QLabel::showEvent(e);
-}
-
-void RunLineLabel::closeEvent(QCloseEvent *e)
-{
-    emit stopAnimation();
-    QLabel::closeEvent(e);
-}
-
-void RunLineLabel::hideEvent(QHideEvent *e)
-{
-    emit stopAnimation();
-    QLabel::hideEvent(e);
-}
-
-void RunLineLabel::timeoutAnimation()
-{
-    static int widthLabel{m_widthText + 50};
-    static int first_x{};
-    static int second_x{first_x + widthLabel};
-    if (first_x < -widthLabel) first_x = widthLabel + second_x;
-    if (second_x < -widthLabel) second_x = widthLabel + first_x;
-    m_lblFirst->setGeometry(QRect(first_x,m_lblFirst->y(),widthLabel,m_lblFirst->height()));
-    m_lblSecond->setGeometry(QRect(second_x,m_lblSecond->y(),widthLabel,m_lblSecond->height()));
-    first_x -= m_speed;
-    second_x -= m_speed;
 }
--- ui/controls/NewsFeedWidget.h
+++ ui/controls/NewsFeedWidget.h
@@ -1,70 +1,28 @@
 #ifndef NEWSFEEDWIDGET_H
 #define NEWSFEEDWIDGET_H
 #include "ClickableLabel.h"
+#include "ClickableRunningLabel.h"
 #include <QHBoxLayout>
-#include <QLabel>
-#include <QDebug>
 #include <QDesktopServices>
-#include <QFontMetrics>
-#include <QTimer>
-#include "Utilz.h"
-#include "UiScaling.h"
 
-class RunLineLabel;
-class AnimationLabel;
 class NewsFeedWidget:public QLabel
 {
     Q_OBJECT
 public:
     NewsFeedWidget(QString a_text, QString a_url, int a_speed, QWidget *parent);
     void updateGeometry();
-    void setUrl(const QString &a_url);
+    void setUrl(const QString &a_url){m_url = a_url;}
     void setText(const QString &a_text);
     void setSpeed(const int a_speed);
     QString text()const;
 protected:
     virtual void resizeEvent(QResizeEvent* ev);
 private:
-    QHBoxLayout*        m_layout{Q_NULLPTR};
-    ClickableLabel*     m_lblClose{Q_NULLPTR};
-    RunLineLabel*       m_lblText{Q_NULLPTR};
-    QWidget*            m_parent{Q_NULLPTR};
-    QSpacerItem*        m_spacerFirst{Q_NULLPTR};
-    QSpacerItem*        m_spacerSecond{Q_NULLPTR};
-};
-
-class RunLineLabel:public ClickableLabel
-{
-    Q_OBJECT
-public:
-    RunLineLabel(QString a_text, QString a_url, QWidget *a_parent);
-    void setUrl(const QString &a_url) {m_url = a_url;}
-    void setText(const QString &a_text);
-    void setSpeed(const int a_speed);
-    QString text()const {return m_text;}
-signals:
-    void startAnimation();
-    void stopAnimation();
-protected:
-    virtual void showEvent(QShowEvent* e);
-    virtual void closeEvent(QCloseEvent* e);
-    virtual void hideEvent(QHideEvent* e);
-private slots:
-    void timeoutAnimation();
-private:
-    void verifyWidth();
-    QString     m_text{};
-    QString     m_url{};
-    QWidget*    m_parent{Q_NULLPTR};
-    int         m_speed{};
-    int         m_widthText{};
-
-            /*if animation start*/
     QHBoxLayout*            m_layout{Q_NULLPTR};
-    QLabel*                 m_lblFirst{Q_NULLPTR};
-    QLabel*                 m_lblSecond{Q_NULLPTR};
-    QTimer*                 m_timer{Q_NULLPTR};
-
+    ClickableLabel*         m_lblClose{Q_NULLPTR};
+    ClickableRunningLabel*  m_lblText{Q_NULLPTR};
+    QSpacerItem*            m_spacerFirst{Q_NULLPTR};
+    QSpacerItem*            m_spacerSecond{Q_NULLPTR};
+    QString                 m_url{};
 };
-
 #endif // NEWSFEEDWIDGET_H
