970ec5c79cd47bc38151ade3676dc6d9aa16d408
Postnickov <postnickov.lyoha@yandex.ru>
2020-09-24 15:06:17 +0300

    [*]IndicatorsFrame

--- vpn/ui/controls/IndicatorsFrame.cpp
+++ vpn/ui/controls/IndicatorsFrame.cpp
@@ -1,21 +1,39 @@
 #include "IndicatorsFrame.h"
 
-IndicatorsFrame::IndicatorsFrame()
+IndicatorsFrame::IndicatorsFrame(QWidget *a_parent)
+    :QFrame(a_parent)
 {
+    m_layout = new QVBoxLayout(this);
 
+    m_authorized = new QCheckBox(tr("Authorized"));
+    m_authorized->setObjectName("autorizedCheckBox");
+    //m_authorized->setEnabled(false);
+
+    m_streamOpened = new QCheckBox(tr("Stream Established"));
+    m_streamOpened->setObjectName("streamOpenedCheckBox");
+
+    m_virtualNetwork = new QCheckBox(tr("Network Settings Present"));
+    m_virtualNetwork->setObjectName("virtualNetworkCheckBox");
+
+    m_layout->addWidget(m_authorized);
+    m_layout->addWidget(m_streamOpened);
+    m_layout->addWidget(m_virtualNetwork);
+    m_layout->setContentsMargins(0,0,0,0);
+    m_layout->setSpacing(0);
+    setLayout(m_layout);
 }
 
 void IndicatorsFrame::setAuthorized(bool a_authorized)
 {
-
+    m_authorized->setChecked(a_authorized);
 }
 
 void IndicatorsFrame::setStreamOpened(bool a_streamOpened)
 {
-
+    m_streamOpened->setChecked(a_streamOpened);
 }
 
 void IndicatorsFrame::setVirtualNetwork(bool a_virtualNetwork)
 {
-
+    m_virtualNetwork->setChecked(a_virtualNetwork);
 }
--- vpn/ui/controls/IndicatorsFrame.h
+++ vpn/ui/controls/IndicatorsFrame.h
@@ -2,17 +2,24 @@
 #define INDICATORSFRAME_H
 
 #include <QFrame>
+#include <QVBoxLayout>
+#include <QCheckBox>
 
 class IndicatorsFrame : public QFrame
 {
     Q_OBJECT
 public:
-    IndicatorsFrame();
+    IndicatorsFrame(QWidget* a_parent = nullptr);
 
 public slots:
     void setAuthorized(bool a_authorized = true);
     void setStreamOpened(bool a_streamOpened = true);
     void setVirtualNetwork(bool a_virtualNetwork = true);
+private:
+    QVBoxLayout*    m_layout{Q_NULLPTR};
+    QCheckBox*      m_authorized{Q_NULLPTR};
+    QCheckBox*      m_streamOpened{Q_NULLPTR};
+    QCheckBox*      m_virtualNetwork{Q_NULLPTR};
 };
 
 #endif // INDICATORSFRAME_H
