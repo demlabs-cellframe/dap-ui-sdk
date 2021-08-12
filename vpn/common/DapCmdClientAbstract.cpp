82d5767de851e213ad6272f7632853da063966d7
Segaman68k <segaman68k@gmail.com>
2021-08-12 13:44:12 +0300

    optimized. documented.

--- vpn/common/DapCmdClientAbstract.cpp
+++ vpn/common/DapCmdClientAbstract.cpp
@@ -1,27 +1,62 @@
+/* INCLUDES */
 #include "DapCmdClientAbstract.h"
+
+/********************************************
+ * CONSTRUCT/DESTRUCT
+ *******************************************/
+
+DapCmdClientAbstract::DapCmdClientAbstract (DapJsonCmdType cmd, QObject *parent):
+  DapCmdAbstract (Side::CLIENT, cmd, parent)
+{
+
+}
 
 DapCmdClientAbstract::~DapCmdClientAbstract()
 {
 
 }
 
+/********************************************
+ * OVERRIDES
+ *******************************************/
 
-void DapCmdClientAbstract::handle(const QJsonObject* params)
+void DapCmdClientAbstract::handle (const QJsonObject *params)
 {
-    if(params->contains("error")) {
-        auto errorVal = params->value("error");
-        if(!errorVal.toObject().contains("code") ||
-                !errorVal.toObject().contains("message")) {
-            qCritical() << "Bad error response from service. Must contains: code and message";
-            return;
-        }
-        int code = errorVal.toObject()["code"].toInt();
-        QString message = errorVal.toObject()["message"].toString();
-        m_errorObject = params;
-        handleError(code, message);
-        m_errorObject = Q_NULLPTR;
-        return;
+  /* handle result, if no error */
+  if (!params->contains ("error"))
+    return handleResult (*params);
+
+  /* get error object and check fields presence */
+  auto errorVal = params->value ("error").toObject();
+  if (!errorVal.contains ("code")
+      || !errorVal.contains ("message"))
+    {
+      qCritical() << "Bad error response from service. Must contains: code and message";
+      return;
     }
 
-    handleResult(*params);
+  /* get fields and handle error */
+  int code        = errorVal.value ("code").toInt();
+  QString message = errorVal.value ("message").toString();
+  m_errorObject   = params;
+
+  handleError (code, message);
+
+  m_errorObject   = Q_NULLPTR;
+  return;
+}
+
+void DapCmdClientAbstract::sendCmd (const QJsonObject *obj)
+{
+//  if (obj == Q_NULLPTR)
+//    {
+//      qDebug() << "Send command" << commandToString (cmd());
+//    }
+//  else
+//    {
+//      qDebug() << "Send command" << commandToString (cmd())
+//               << "params:"      << *obj;
+//    }
+
+  DapCmdAbstract::sendCmd (obj);
 }
