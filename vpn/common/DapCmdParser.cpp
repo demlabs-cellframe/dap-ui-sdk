4853d875ea0555268804fa1cccab1334874c1ba8
Segaman68k <segaman68k@gmail.com>
2021-08-13 15:03:20 +0300

    documented parser. added feature to dump messages.

--- vpn/common/DapCmdParser.cpp
+++ vpn/common/DapCmdParser.cpp
@@ -1,34 +1,101 @@
+/* INCLUDES */
 #include "DapCmdParser.h"
+#include <QFile>
+#include <QDateTime>
 
-DapCmdParser::DapCmdParser(QTcpSocket* sockCtl, QObject *parent)
-    : QObject(parent)
-{    
-    m_sockCtl = sockCtl;
-    connect(m_sockCtl, &QTcpSocket::readyRead,
-            this, &DapCmdParser::onCtlReadReady);
+/// TODO : unit test
+
+/* VARS */
+static QFile *file = nullptr;
+
+/********************************************
+ * CONSTRUCT/DESTRUCT
+ *******************************************/
+
+DapCmdParser::DapCmdParser (QTcpSocket *sockCtl, QObject *parent, bool dumpMessages)
+  : QObject (parent),
+    m_sockCtl (sockCtl),
+    m_dumpMessages (dumpMessages)
+{
+  /* connect signal */
+  if (m_sockCtl)
+    connect (m_sockCtl, &QTcpSocket::readyRead,
+             this, &DapCmdParser::onCtlReadReady);
+  if (m_dumpMessages)
+    {
+      auto dt = QDateTime::currentDateTime()
+                .toString ("HH:mm:ss_dd-MM-yyyy");
+      file = new QFile (QString("/opt/kelvpn/dump/") + dt + ".rdmp");
+      file->open (QIODevice::WriteOnly);
+    }
 }
+
+/********************************************
+ * PUBLIC SLOTS
+ *******************************************/
 
 void DapCmdParser::onCtlReadReady()
 {
-    QByteArray readBytes = m_sockCtl->readAll();
-    int nInd;
+  /* vars */
+  int nInd;
 
+  /* read message */
+  auto readBytes  = m_sockCtl->readAll();
+
+  /* read in cycle */
 lb_read_str:
-    if((nInd=readBytes.indexOf("}\n\n")) == -1 ) {
-        qDebug() << "No CR symbol";
-        readBuffer += readBytes;
-        if(readBuffer.size() > MAX_BUFFER_SIZE) {
-            qWarning() << "Buffer is full. All data is dropped";
-            return;
+
+  /* check for packet end symbol presence */
+  if ((nInd = readBytes.indexOf ("}\n\n")) == -1)
+    {
+      /* warn */
+      qDebug() << "No CR symbol";
+      readBuffer += readBytes;
+
+      /* check buffer overflow */
+      if (readBuffer.size() > MAX_BUFFER_SIZE)
+        {
+          qWarning() << "Buffer is full. All data is dropped";
+          return;
         }
-    } else {
-        nInd+=2; // move idx to second \n
-        readBuffer += readBytes.left(nInd);
-        emit cmdReady(readBuffer);
-        readBytes = readBytes.mid(nInd+1);
-        readBuffer.clear();
-        if(readBytes.length() > 0) {
-            goto lb_read_str;
+    }
+  else
+    {
+      /* move idx to second \n */
+      nInd        += 2;
+
+      /* get message and send as signal argument */
+      readBuffer  += readBytes.left (nInd);
+      emit cmdReady (readBuffer);
+
+      /* dump message, if required */
+      if (m_dumpMessages && file)
+        {
+          if (file->isOpen())
+            {
+              auto dt = QDateTime::currentDateTime()
+                        .toString ("HH:mm:ss_dd-MM-yyyy");
+
+              file->write (
+                QString ("@@%1@@\n").arg (dt)
+                .toLocal8Bit());
+              file->write (readBuffer);
+              file->flush();
+            }
+        }
+
+      /* cut out beginning */
+      readBytes   = readBytes.mid (nInd + 1);
+
+      /* clear buffer */
+      readBuffer.clear();
+
+      /* loop, if another message exists */
+      if (readBytes.length() > 0)
+        {
+          goto lb_read_str;
         }
     }
 }
+
+/*-----------------------------------------*/
--- vpn/common/DapCmdParser.h
+++ vpn/common/DapCmdParser.h
@@ -1,23 +1,62 @@
 #ifndef DAPCMDPARSER_H
 #define DAPCMDPARSER_H
 
+/* INCLUDES */
 #include <QObject>
 #include <QTcpSocket>
 
+/****************************************//**
+ * @brief message receiver for GUI
+ * @note if dumpMessages is true, class
+ * will save messages into "/opt/kelvpn/dump"
+ * @note if sockCtl is nullptr, class will
+ * simulate socket messages for unit test.
+ * Activate new messages by sending signal
+ * to onCtlReadReady()
+ *******************************************/
+
 class DapCmdParser : public QObject
 {
-    Q_OBJECT
+  Q_OBJECT
+
+  /****************************************//**
+   * @name CONSTRUCT/DESTRUCT
+   *******************************************/
+  /// @{
 public:
-    explicit DapCmdParser(QTcpSocket* sockCtl,
-                          QObject *parent = nullptr);
+  explicit DapCmdParser (
+    QTcpSocket *sockCtl,
+    QObject *parent = nullptr,
+    bool dumpMessages = false);
+  /// @}
+
+  /****************************************//**
+   * @name PRIVATE VARS
+   *******************************************/
+  /// @{
 private:
-    const static uint16_t MAX_BUFFER_SIZE = 10000;
-    QTcpSocket * m_sockCtl;
-    QByteArray readBuffer;
+  static const uint16_t MAX_BUFFER_SIZE = 10000;
+  QTcpSocket *m_sockCtl;
+  QByteArray readBuffer;
+  bool m_dumpMessages;
+  /// @}
+
+  /****************************************//**
+   * @name SIGNALS
+   *******************************************/
+  /// @{
 signals:
-    void cmdReady(QByteArray ba);
+  void cmdReady (QByteArray ba);
+  /// @}
+
+  /****************************************//**
+   * @name PUBLIC SLOTS
+   *******************************************/
+  /// @{
 public slots:
-    void onCtlReadReady();
+  void onCtlReadReady();
+  /// @}
 };
 
+/*-----------------------------------------*/
 #endif // DAPCMDPARSER_H
