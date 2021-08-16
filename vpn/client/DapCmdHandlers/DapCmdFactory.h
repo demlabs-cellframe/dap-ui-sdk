16c1530a95fed9a1f78eeaae8f3675cc9fba2928
Segaman68k <segaman68k@gmail.com>
2021-08-16 20:36:32 +0300

    rewrote factory for flexability. documented.

--- vpn/client/DapCmdHandlers/DapCmdFactory.h
+++ vpn/client/DapCmdHandlers/DapCmdFactory.h
@@ -5,17 +5,30 @@
 #include "DapCmdServiceAbstract.h"
 #include <QMap>
 #include <QMutex>
+#include <QScopedPointer>
+#include <typeinfo>
 
 /****************************************//**
- * @brief instance container
- * for DapCmdServiceAbstract child classes
- * @note for service use
+ * @brief template instance container factory
+ * for DapCmdServiceAbstract child classes.
+ * @note for service use.
+ * @note class Item is thread-safe.
  *
- * - @ref instance() to get singleton instance
+ * To use this factory: create template Item
+ * instance providing the name of class and
+ * use it's operators to get instance pointer.
  *
- * - @ref operator[] to instanciate or/and get instance of DapCmd
+ * Instance of provided class will be created
+ * once (singleton), will exist safely inside
+ * current thread and can be accessed safely
+ * via instantiating template class Item with
+ * provided class name, which acts like
+ * QMutexLock, but with dereferencind
+ * operators ( operator->() and operator&() )
  *
- * - @ref get(const Type &t) same as operator[], but also dynamicaly cast return
+ * @example DapCmdFactory::Item<DapCmdStates>()->sendCmdStates();
+ * @example using Factory = DapCmdFactory::Item<T>; Factory<DapCmdStates>()->sendCmdStates();
+ * @example {DapCmdFactory::Item<DapCmdStates> op; op->sendCmdStates();}
  *
  * @date 16.08.2021
  * @author mikhail.shilenko
@@ -28,24 +41,95 @@ class DapCmdFactory
    *******************************************/
   /// @{
 public:
-  enum Type
+  template<class T>
+  class Item
   {
-    AndroidTunnel,
-    Auth,
-    Connect,
-    LastConnectionData,
-    News,
-    PingServer,
-    Purchase,
-    SendBugReport,
-    ServerData,
-    ServerList,
-    SignUp,
-    States,
-    Stats,
-    TunTap,
-    UserData,
+    QMutexLocker *locker;
+    T *ptr;
+  public:
+    Item()
+    {
+      auto &i = DapCmdFactory::get<T>();
+      locker  = new QMutexLocker(i.getLock());
+      ptr     = dynamic_cast<T*> (i.getValue());
+    }
+    ~Item()
+    {
+      delete locker;
+    }
+    T *operator->() {return ptr;}
+    T *operator &() {return ptr;}
   };
+  /// @}
+
+  /****************************************//**
+   * @name PROTECTED DEFS
+   *******************************************/
+  /// @{
+protected:
+  class Node
+  {
+    /* VARS */
+    QMutex *lock;
+    DapCmdServiceAbstract *value;
+
+    /* CONSTRUCT/DESTRUCT */
+  public:
+    Node (DapCmdServiceAbstract *s)
+      : lock (new QMutex)
+      , value (s)
+    {
+
+    }
+    Node () = delete;
+    Node (const Node &) = delete;
+    Node (Node &&r)
+    {
+      if(&r == this)
+        return;
+      lock    = r.lock;
+      value   = r.value;
+      r.lock  = nullptr;
+      r.value = nullptr;
+    }
+    ~Node()
+    {
+      auto tl = lock;
+      auto tv = value;
+      lock    = nullptr;
+      value   = nullptr;
+      delete tl;
+      delete tv;
+    }
+
+    /* METHODS */
+    template<class T>
+    static Node *create()
+    {
+      return new Node(new T);
+    }
+
+    /* OPERATORS */
+    Node &operator=(const Node&) = delete;
+    Node &operator=(Node &&r)
+    {
+      lock    = r.lock;
+      value   = r.value;
+      r.lock  = nullptr;
+      r.value = nullptr;
+      return *this;
+    }
+    DapCmdServiceAbstract *getValue() const {return value;};
+    QMutex *getLock() const {return lock;};
+  };
+  /// @}
+
+  /****************************************//**
+   * @name FRIENDS
+   *******************************************/
+  /// @{
+  template<typename T>
+  friend class Item;
   /// @}
 
   /****************************************//**
@@ -53,60 +137,40 @@ public:
    *******************************************/
   /// @{
 private:
-  QMap<Type, DapCmdServiceAbstract*> m_cmds; ///< container
+  static QMap<QString, Node*> m_cmds;  ///< container
   static QMutex m_mutex;
   /// @}
 
   /****************************************//**
-   * @name CONSTRUCT/DESTRUCT
+   * @name PROTECTED METHODS
    *******************************************/
   /// @{
-private:
-  /* define */
-
-  explicit DapCmdFactory();
-  ~DapCmdFactory();
-
-  /* delete */
-
-  DapCmdFactory(const DapCmdFactory&) = delete;
-  DapCmdFactory(DapCmdFactory&&) = delete;
-  /// @}
-
-  /****************************************//**
-   * @name STATIC METHODS
-   *******************************************/
-  /// @{
-public:
-  /// get singleton instance
-  static DapCmdFactory *instance();
-  /// @}
-
-  /****************************************//**
-   * @name PUBLIC METHODS
-   *******************************************/
-  /// @{
-public:
-  /// overlap operator[] for usability
-  template<typename item>
-  item *get (const Type &t)
+protected:
+  /// get item from map
+  template<typename T>
+  static Node &get ()
   {
-    return dynamic_cast<item *> ((*this)[t]);
+    QMutexLocker l(&m_mutex);
+    auto name = typeid (T).name();
+
+    /* return, if present */
+    if (m_cmds.contains (name))
+      return *m_cmds[name];
+
+    /* create & return */
+    return *(m_cmds[name] = Node::create<T>());
   }
   /// @}
 
   /****************************************//**
-   * @name PUBLIC OPERATORS
+   * @name DELETE
    *******************************************/
   /// @{
 public:
-  /* define */
-
-  /// get cmd handler by type
-  DapCmdServiceAbstract *operator [] (const Type &t);
-
-  /* delete */
-
+  DapCmdFactory() = delete;
+  DapCmdFactory(const DapCmdFactory&) = delete;
+  DapCmdFactory(DapCmdFactory&&) = delete;
+  ~DapCmdFactory() = delete;
   DapCmdFactory &operator=(const DapCmdFactory&) = delete;
   DapCmdFactory &operator=(DapCmdFactory&&) = delete;
   /// @}
