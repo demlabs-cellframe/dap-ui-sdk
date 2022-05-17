3ae194911e73e3294279d678b1e45108282b710b
Stanislav Ratseburzhinsky <rsl@viedu.ru>
2022-05-17 08:30:36 +0500

    bugs-6254|[+]Chart color management moved to style.css. Added control of distance between text characters

--- widgets/dapguigraphicsviewstylemanager.cpp
+++ widgets/dapguigraphicsviewstylemanager.cpp
@@ -6,6 +6,19 @@
  * CONSTRUCT/DESTRUCT
  *******************************************/
 
+QString fetchColor(const QString &style, const QString key)
+{
+    QString a_style(style);
+    a_style = a_style.remove(" ");
+    QString regExp(key + ":([#0-9a-fA-F]+);");
+    int a = a_style.indexOf(QRegExp(regExp));
+    if (a < 0)
+        return nullptr;
+    int b = a_style.indexOf(QRegExp(":"), a) + 1;
+    int c = a_style.indexOf(QRegExp(";"), a);
+    return a_style.mid(b, c-b);
+}
+
 DapGuiGraphicsViewStyleManager::DapGuiGraphicsViewStyleManager (QWidget *parent)
   : DapGuiStyleManager (parent)
 {
@@ -45,21 +58,30 @@ void DapGuiGraphicsViewStyleManager::forcedGraphicsViewStyleUpdate()
   if (!gv || !scene)
     return;
 
+  auto style = styleByClassName ("graphics");
+
   /* color */
   struct ColorStruct
   {
-    const char
-      *background,
-      *download,
-      *upload;
+    QString
+      background,
+      download,
+      upload;
   };
   static QMap<QString, ColorStruct> mapColor =
+  // default value
   {
     { "light", {"#f7f8fa", "#D61F5D", "#2D1FD6"} },
     { "dark",  {"#2f2e42", "#FF22A2", "#9F83FF"} },
   };
   auto dir    = themeDir();
   auto color  = mapColor.value (dir, mapColor.first());
+  if (style.contains("colorBackground"))
+      color.background = fetchColor(style, "colorBackground");
+  if (style.contains("colorChartDownload"))
+      color.download = fetchColor(style, "colorChartDownload");
+  if (style.contains("colorChartUpload"))
+      color.upload = fetchColor(style, "colorChartUpload");
   gv->form()->setProperty ("colorBackground",     color.background);
   gv->form()->setProperty ("colorChartDownload",  color.download);
   gv->form()->setProperty ("colorChartUpload",    color.upload);
@@ -68,7 +90,7 @@ void DapGuiGraphicsViewStyleManager::forcedGraphicsViewStyleUpdate()
   auto rect   = scene->sceneRect();
   rect.setSize (size);
   scene->setSceneRect (rect);
-  scene->addRect (rect, QPen(Qt::transparent), QBrush (color.background));
+  scene->addRect (rect, QPen(Qt::transparent), QBrush (qPrintable(color.background)));
 }
 
 /*-----------------------------------------*/
