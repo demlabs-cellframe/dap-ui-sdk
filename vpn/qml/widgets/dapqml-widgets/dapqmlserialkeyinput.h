75ce90ff7ef4bcc76d1beb5cd16d2583d0a5e25d
Segaman68k <segaman68k@gmail.com>
2022-06-15 17:33:26 +0400

    documented serial key input widget

--- vpn/qml/widgets/dapqml-widgets/dapqmlserialkeyinput.h
+++ vpn/qml/widgets/dapqml-widgets/dapqmlserialkeyinput.h
@@ -5,7 +5,27 @@
 #include <QQuickItem>
 
 /****************************************//**
- * @brief serial key input field widget
+ * @brief serial key input field filter bridge
+ *
+ * This class used to provide bridge connection
+ * between QML Item Input field and simple
+ * event filter as a callback
+ *
+ * Usualy this helps to control Serial Key input
+ * on different platfroms (Desktop/Android)
+ *
+ * To start working as a event filter:
+ * -Create instance of this class
+ * -Execute "setup" method providing a Input QML Item
+ * -Set a callback for event filtering
+ *
+ * After this every time QML Item gets a event
+ * callback will be called providing this class pointer
+ * and event pointer, as if this was a simple
+ * QObject event filter
+ *
+ * @note setup can only called once. if called again - behavior is undefined
+ *
  * @date 08.04.2022
  * @author Mikhail Shilenko
  *******************************************/
@@ -26,6 +46,7 @@ class DapQmlSerialKeyInput : public QObject
    *******************************************/
   /// @{
 public:
+  /// @brief callback for event filtering
   typedef bool (*cbInputMethodEvent) (DapQmlSerialKeyInput *widget, QEvent *event);
   /// @}
 
@@ -34,9 +55,9 @@ public:
    *******************************************/
   /// @{
 protected:
-  cbInputMethodEvent m_callbackEvent; ///< called at event
-  QString m_commiting;
-  QObject *m_qmlItem;
+  cbInputMethodEvent m_callbackEvent; ///< called at event filter
+  QString m_commiting;                ///< commiting string for android
+  QObject *m_qmlItem;                 ///< pointer to attached qml item
   /// @}
 
   /****************************************//**
@@ -60,18 +81,30 @@ signals:
    *******************************************/
   /// @{
 public:
+  /// @brief get commiting string
+  /// @note for android only
   const QString &commiting() const;
+  /// @brief set commiting string
+  /// @note for android only
   void setCommiting(const QString &newCommiting);
 
+  /// @brief get input field text
   QString text() const;
+  /// @brief set input field text
   void setText (const QString &a_text);
 
+  /// @brief get attached qml item
   QObject *qmlItem() const;
 
+  /// @brief get installed callback
   Q_INVOKABLE cbInputMethodEvent callbackEvent() const;
+  /// @brief install a callback for event filter
   Q_INVOKABLE void setCallbackEvent(cbInputMethodEvent newCallbackInputMethodEvent);
+  /// @brief attach qml item
   Q_INVOKABLE void setup (QObject *a_qmlItem);
+  /// @brief force focus on input field
   Q_INVOKABLE void setFocus();
+  /// @brief simulate backspace press behavior
   Q_INVOKABLE bool sendBackspaceEvent();
   /// @}
 
