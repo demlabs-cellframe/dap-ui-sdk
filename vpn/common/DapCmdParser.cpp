ce30cdd4a85f4a8852020d6ec49ade47c4c63634
danil.martynenko <danil.martynenko@demlabs.net>
2024-09-20 11:26:23 +0300

    [*] Removed excessive logging, added buffer content output on overflow for better diagnostics

--- vpn/common/DapCmdParser.cpp
+++ vpn/common/DapCmdParser.cpp
@@ -13,38 +13,32 @@ DapCmdParser::DapCmdParser(QTcpSocket* sockCtl, QObject *parent)
 void DapCmdParser::onCtlReadReady()
 {
     QByteArray readBytes = m_sockCtl->readAll();
-    qDebug() << "Received data from socket:" << readBytes;
     int nInd;
 
     // Loop to handle multiple potential commands in the incoming data
     while (!readBytes.isEmpty()) {
         // Check if the end of the command sequence (}\n\n) is present
         if ((nInd = readBytes.indexOf("}\n\n")) == -1) {
-            qDebug() << "No end of command sequence found";
             // Append received data to the buffer
             readBuffer += readBytes;
-            qDebug() << "Current buffer size:" << readBuffer.size();
 
             // Check if buffer size exceeds the maximum allowed
             if (readBuffer.size() > MAX_BUFFER_SIZE) {
-                qWarning() << "Buffer is full. All data is dropped";
+                qWarning() << "Buffer is full. All data is dropped. Buffer content: " << readBuffer;
                 readBuffer.clear(); // Clear buffer to prevent overflow
             }
             break;
         } else {
             // Command sequence found, processing the command
             nInd += 2; // Move index to the second \n
-            qDebug() << "End of command sequence found at index:" << nInd;
 
             // Extract the complete command from the data
             readBuffer += readBytes.left(nInd);
-            qDebug() << "Full command received:" << readBuffer;
             emit cmdReady(readBuffer);
 
             // Remove processed command from the remaining data
             readBytes = readBytes.mid(nInd + 1);
             readBuffer.clear();
-            qDebug() << "Buffer cleared, processing remaining data if any.";
         }
     }
 }
