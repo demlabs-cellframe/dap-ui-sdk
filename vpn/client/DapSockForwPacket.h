538ea27884df6f35357d8705013da0aa61d40c87
konstantin <konstantin.kuharenko@demlabs.net>
2025-11-15 23:37:43 +0300

    add stream

--- vpn/client/DapSockForwPacket.h
+++ vpn/client/DapSockForwPacket.h
@@ -2,9 +2,13 @@
 #define DAPSOCKFORWPACKET_H
 
 #include <QtGlobal>
+
+// CRITICAL: This structure MUST match the old DapStreamChChainVpnPacket.h::PacketHdr
+// Server expects exactly this format with usage_id field!
 struct DapSockForwPacketHdr{
-    int socket_id;
-    quint32 op_code;
+    int socket_id;        // Socket ID (4 bytes)
+    quint32 op_code;      // Operation code (4 bytes): 0xbc = send, 0xf8 = receive
+    quint32 usage_id;     // Usage ID (4 bytes) - REQUIRED by server protocol!
     union
     {
         struct
@@ -12,17 +16,17 @@ struct DapSockForwPacketHdr{
             quint32 addr_size;
             quint16 port;
             quint16 padding;
-        } op_connect;
+        } __attribute__((packed)) op_connect;
         struct
         {
             quint32 data_size;
             quint32 padding;
-        } raw;   // Default access to raw OP data
+        } __attribute__((packed)) raw;   // Default access to raw OP data
         struct
         {
             quint32 data_size;
             quint32 padding;
-        } op_data;
+        } __attribute__((packed)) op_data;
     };
 } __attribute__((packed));
 
